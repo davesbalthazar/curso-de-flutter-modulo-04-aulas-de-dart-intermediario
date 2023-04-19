f9dbc8ff02da8124a1b3d6e133348163b97a229d
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-19 13:05:25 -0300

    Aula 10 - Looping em Dart com for

---
+++ dart-aula-10-looping-em-dart-com-for.dart
@@ -0,0 +1,44 @@
+///
+/// Curso de Flutter e Dart
+/// Daves Balthazar
+/// Aula 10 - Looping em Dart com for
+/// https://www.youtube.com/watch?v=0hR_20jaOTk&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=13
+///
+void main() {
+  for (double a = 10; a >= 1; a -= 0.5) {
+    print(a);
+  }
+
+  /* 
+  for( int i=0; i<3; i++ ) {
+    
+    for( int j=0; j<3; j++ ) {
+    
+        print( '($i,$j)' );
+    }
+  }
+*/
+  /*
+  for( int i=10; i>=1; i--) {
+    print( i );    
+  }
+  
+
+  for( int i=1; i<=10; i++ ) {
+    print( i );
+  }
+  
+  
+
+  print( '1' );
+  print( '2' );
+  print( '3' );
+  print( '4' );
+  print( '5' );
+  print( '6' );
+  print( '7' );
+  print( '8' );
+  print( '9' );
+  print( '10' );
+*/
+}
